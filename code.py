578ac615a47aabcbadf5fedb7e28b9fd340667a0
Vladimir <krechetvova@gmail.com>
2024-10-18 21:14:30 +0700

    second_commit

--- code.py
+++ code.py
@@ -10,3 +10,8 @@ if (result == 5):
     print('Результат: ' + str(result));
 else:
     print('Ошибка')
+
+name = input('Как тебя зовут?')
+age = int(input('Сколько тебе лет?'))
+result = 'Привет ' + name + ', тебе ' + str(age) + ' лет.'
+print(result)
