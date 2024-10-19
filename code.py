7f756a5594c785f7c1b3fb5bf29d4bd95c6361bc
Vladimir <krechetvova@gmail.com>
2024-10-19 09:59:52 +0700

    if_else

--- code.py
+++ code.py
@@ -2,3 +2,15 @@ first_name = 'Vladimir'
 last_name = 'Ivanov'
 
 print('Меня зовут ' + first_name + ' ' + last_name)
+
+
+a = 5
+b = 10
+if (a==b):
+    print("Равно")
+elif (a < b):
+    print("b больше чем a")
+elif (a > b):
+    print("a больше чем b")
+else:
+    print("Ошибка")
